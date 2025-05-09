b94e3374a3600be82d07f2060ea298ecdca4c05e
porteiro1 <minecraftmaua@gmail.com>
2025-05-09 09:07:48 -0300

    test xomiit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         boolean rodando = true;
         Hotel hotel = new Hotel("Hotel legal");
